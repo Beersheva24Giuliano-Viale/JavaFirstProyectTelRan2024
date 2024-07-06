231f0e62bb086923b9f5c1765ebebc759ed7e2ac
GiulianoViale <giulianoviale@hotmail.com>
2024-07-06 16:02:17 +0300

    Adding individual tests and methods

--- src/main/java/telran/introduction/Calculations.java
+++ src/main/java/telran/introduction/Calculations.java
@@ -1,8 +1,47 @@
 package telran.introduction;
 
 public class Calculations {
-    public static int sum(int op1, int op2){
-        //TODO
-        return 0;
+    public static int sum(int op1, int op2) {
+        int res = op1 + op2;
+        return res;
+    }
+
+    public static int multiply(int op1, int op2) {
+        int res = op1 * op2;
+        return res;
+    }
+
+    public static int divide(int op1, int op2) {
+        int res = op1 / op2;
+        return res;
+    }
+
+    public static int substract(int op1, int op2) {
+        int res = op1 - op2;
+        return res;
+    }
+
+    public static int sumOfDigits(int number) {
+        // i convert the number to a string so i can use the string method that counts
+        // de value of it
+        String numberStr = String.valueOf(number);
+        return numberStr.length();
+    }
+
+    public static int maxDigit(int number) {
+        number = Math.abs(number);/* converting the number into positive in case that is negative */
+        int biggestDigit = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            biggestDigit = Math.max(digit, biggestDigit);
+            number = number / 10;
+        }
+        return biggestDigit;
+    }
+
+    public static boolean isDividedOn(int number, int divider) {
+       if (number%divider==0) {
+            return true;
+       }else return false;
     }
 }
--- src/test/java/telran/introduction/CalculationsTest.java
+++ src/test/java/telran/introduction/CalculationsTest.java
@@ -1,5 +1,53 @@
 package telran.introduction;
+
+/* import static org.junit.jupiter.api.Assertions.assertEquals; THIS WAY YOU HAVE TO HAND PICK ALL THE METHODS, WHIT ASTERISCO YOU SELECT ALL*/
+import static org.junit.jupiter.api.Assertions.*;
+import static telran.introduction.Calculations.*;
+
+import org.junit.jupiter.api.Test;
+
 public class CalculationsTest {
-
-
+    @Test
+    void sumTest() {
+        // assertEquals : the first parameter is the result that is expected in the
+        // test, the second parameter is the action of the method tested.
+        assertEquals(4, sum(2, 2));
+    }
+    @Test
+    void multTest(){
+        assertEquals(0, multiply(2, 0));
+        assertEquals(6, multiply(3, 2));
+    }
+    @Test
+    void divTest(){
+        assertEquals(0, divide(0, 4));
+        assertEquals(2, divide(6, 3));
+    }
+    @Test
+    void subsTest(){
+        assertEquals(-3, substract(2, 5));
+        assertEquals(5, substract(10, 5));
+    }
+    @Test
+    void sumDigitsTest(){
+        assertEquals(1 ,sumOfDigits(1));
+        assertEquals(2 ,sumOfDigits(12));
+        assertEquals(3 ,sumOfDigits(123));
+        assertEquals(4 ,sumOfDigits(5436));
+    }
+    @Test
+    void maxDigTest(){
+        assertEquals(5, maxDigit(3425));
+        assertEquals(6, maxDigit(34162));
+    }
+    @Test
+    void isDivTestTrue(){
+       assertTrue(isDividedOn(6, 3)); 
+       assertTrue(isDividedOn(6, 2)); 
+    }
+    @Test
+    void isDivTestFalse(){
+        assertFalse(isDividedOn(6, 5));
+        assertFalse(isDividedOn(6, 4));
+    }
 }
